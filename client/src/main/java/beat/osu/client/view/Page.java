c194eeb531abedfad44285a84224a244ab1c5a64
Wenfuuu <137779146+Wenfuuu@users.noreply.github.com>
2025-05-30 13:22:18 +0700

    feat: custom cursor

--- client/src/main/java/beat/osu/client/view/Page.java
+++ client/src/main/java/beat/osu/client/view/Page.java
@@ -2,6 +2,7 @@ package beat.osu.client.view;
 
 import beat.osu.client.Main;
 import beat.osu.client.helper.InputManager;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -22,6 +23,10 @@ public abstract class Page {
     }
 
     private void showPage(){
+        Image cursorImage = new Image(Objects.requireNonNull(Main.class
+                .getResource("/assets/images/cursor.png")).toExternalForm());
+        scene.setCursor(new ImageCursor(cursorImage, cursorImage.getWidth() / 2, cursorImage.getHeight() / 2));
+
         stage.setScene(scene);
         stage.getIcons().add(new Image(Objects.requireNonNull(Main.class
                 .getResource("/assets/logo/osu_logo.png")).toExternalForm()));
