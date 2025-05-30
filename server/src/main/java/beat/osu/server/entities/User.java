e3cffd2e905c45104142f7894fff00133ea7e39a
Artificed <paultsai.id@gmail.com>
2025-05-30 20:22:46 +0700

    feat: User model and repo for server

---
+++ server/src/main/java/beat/osu/server/entities/User.java
@@ -0,0 +1,21 @@
+package beat.osu.server.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private String countryCode;
+    private byte[] profilePicture;
+    private int performance;
+    private double accuracy;
+    private int playCount;
+    private int level;
+}
---
+++ server/src/main/java/beat/osu/server/repositories/UserRepository.java
@@ -0,0 +1,55 @@
+package beat.osu.server.repositories;
+
+import beat.osu.server.database.Connect;
+import beat.osu.server.entities.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserRepository {
+    private final Connection conn;
+
+    public UserRepository() {
+        this.conn = Connect.getInstance().getConn();
+    }
+
+    public void InsertUser(String username, String email, String password, String countryCode) {
+        String query = "INSERT INTO users (username, email, password_hash, country_code, performance, accuracy, play_count, level) " +
+                "VALUES (?, ?, ?, ?, 0, 0.00, 0, 1);";
+        try {
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setString(1, username);
+            statement.setString(2, email);
+            statement.setString(3, password);
+            statement.setString(4 , countryCode);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findUserByUsername(String username) {
+        String query = "SELECT * FROM users WHERE username = ?;";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, username);
+            var rs = ps.executeQuery();
+
+            return new User(
+                    rs.getInt("id"),
+                    rs.getString("username"),
+                    rs.getString("email"),
+                    rs.getString("password_hash"),
+                    rs.getString("country_code"),
+                    rs.getBytes("profile_picture"),
+                    rs.getInt("performance"),
+                    rs.getDouble("accuracy"),
+                    rs.getInt("play_count"),
+                    rs.getInt("level")
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
