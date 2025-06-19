c7ceca805b55b145f39cbc8e749ffd22cacf05fc
Wenfuuu <137779146+Wenfuuu@users.noreply.github.com>
2025-06-19 20:28:02 +0700

    feat: added load replay in replay utils

--- client/src/main/java/beat/osu/client/utils/ReplayUtils.java
+++ client/src/main/java/beat/osu/client/utils/ReplayUtils.java
@@ -3,9 +3,7 @@ package beat.osu.client.utils;
 import beat.osu.client.events.game.ReplayEventData;
 import beat.osu.client.helper.ResourceManager;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 public class ReplayUtils {
@@ -23,5 +21,30 @@ public class ReplayUtils {
             fw.write(line);
         }
         fw.close();
+    }
+
+    public static ArrayList<ReplayEventData> loadReplay(String osrFileName) throws IOException {
+        File replayDir = ResourceManager.getReplayDirectory();
+        File osrFile = new File(replayDir, osrFileName);
+        ArrayList<ReplayEventData> replayEvents = new ArrayList<>();
+
+        if (!osrFile.exists()) {
+            throw new IOException("Replay file not found: " + osrFileName);
+        }
+
+        BufferedReader br = new BufferedReader(new FileReader(osrFile));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split(",");
+            long timeDelta = Long.parseLong(parts[0].split("=")[1]);
+            int x = Integer.parseInt(parts[1].split("=")[1]);
+            int y = Integer.parseInt(parts[2].split("=")[1]);
+            int keyMask = Integer.parseInt(parts[3].split("=")[1].replace(")", ""));
+
+            ReplayEventData event = new ReplayEventData(timeDelta, x, y, keyMask);
+            replayEvents.add(event);
+        }
+
+        return replayEvents;
     }
 }
