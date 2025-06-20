7d520c3debcea804235a064cb9e89d4403fa02d2
Wenfuuu <137779146+Wenfuuu@users.noreply.github.com>
2025-06-20 16:07:43 +0700

    feat: added score entities and repo in server

---
+++ server/src/main/java/beat/osu/server/entities/Score.java
@@ -0,0 +1,27 @@
+package beat.osu.server.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Score {
+    private int id;
+    private int beatmapId;
+    private int userId;
+    private int score;
+    private int highestCombo;
+    private double accuracy;
+    private int perfectHit;
+    private int gekiHit;
+    private int greatHit;
+    private int katuHit;
+    private int goodHit;
+    private int miss;
+    private String grade;
+    private LocalDateTime date;
+}
---
+++ server/src/main/java/beat/osu/server/repositories/ScoreRepository.java
@@ -0,0 +1,93 @@
+package beat.osu.server.repositories;
+
+import beat.osu.server.database.Connect;
+import beat.osu.server.entities.Score;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class ScoreRepository {
+    private final Connection conn;
+
+    public ScoreRepository() {
+        this.conn = Connect.getInstance().getConn();
+    }
+
+    public void insertScore(
+            int id,
+            int beatmapId,
+            int userId,
+            int score,
+            int highestCombo,
+            double accuracy,
+            int perfectHit,
+            int gekiHit,
+            int greatHit,
+            int katuHit,
+            int goodHit,
+            int miss,
+            String grade,
+            LocalDateTime date
+    ) {
+        String query = "INSERT INTO beatmap_scores (id, beatmap_id, user_id, score, highest_combo, accuracy, " +
+                "perfect_hit, geki_hit, great_hit, katu_hit, good_hit, miss, grade, date) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        try {
+            PreparedStatement statement = conn.prepareStatement(query);
+            statement.setInt(1, id);
+            statement.setInt(2, beatmapId);
+            statement.setInt(3, userId);
+            statement.setInt(4, score);
+            statement.setInt(5, highestCombo);
+            statement.setDouble(6, accuracy);
+            statement.setInt(7, perfectHit);
+            statement.setInt(8, gekiHit);
+            statement.setInt(9, greatHit);
+            statement.setInt(10, katuHit);
+            statement.setInt(11, goodHit);
+            statement.setInt(12, miss);
+            statement.setString(13, grade);
+            statement.setDate(14, Date.valueOf(date.toLocalDate()));
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<Score> getAllScoresByBeatmapId(int beatmapId) {
+        ArrayList<Score> scores = new ArrayList<>();
+
+        String query = "SELECT * FROM beatmap_scores WHERE beatmap_id = ? ORDER BY score DESC;";
+        try (var statement = conn.prepareStatement(query)) {
+            statement.setInt(1, beatmapId);
+            var rs = statement.executeQuery();
+            while (rs.next()) {
+                Score score = new Score(
+                        rs.getInt("id"),
+                        rs.getInt("beatmap_id"),
+                        rs.getInt("user_id"),
+                        rs.getInt("score"),
+                        rs.getInt("highest_combo"),
+                        rs.getDouble("accuracy"),
+                        rs.getInt("perfect_hit"),
+                        rs.getInt("geki_hit"),
+                        rs.getInt("great_hit"),
+                        rs.getInt("katu_hit"),
+                        rs.getInt("good_hit"),
+                        rs.getInt("miss"),
+                        rs.getString("grade"),
+                        rs.getTimestamp("date").toLocalDateTime()
+                );
+                scores.add(score);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return scores;
+    }
+}
