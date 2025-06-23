05637170d2443f89760299a8afa3c81628f872b9
Wenfuuu <137779146+Wenfuuu@users.noreply.github.com>
2025-06-23 15:20:17 +0700

    fix: added serializable to session data response

--- shared/src/main/java/beat/osu/shared/dto/session/responses/GetSessionDataResponse.java
+++ shared/src/main/java/beat/osu/shared/dto/session/responses/GetSessionDataResponse.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetSessionDataResponse {
+public class GetSessionDataResponse implements Serializable {
     private Object value;
     private String message;
 }
