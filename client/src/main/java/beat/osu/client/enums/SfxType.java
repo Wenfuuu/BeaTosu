dfc42f2280a87014c6ea5510f42a724acbe6e9be
Artificed <paultsai.id@gmail.com>
2025-07-05 17:51:13 +0700

    feat: Added SFX types

---
+++ client/src/main/java/beat/osu/client/enums/SfxType.java
@@ -0,0 +1,17 @@
+package beat.osu.client.enums;
+
+public enum SfxType {
+    MENU_HIT,
+    MENU_HOVER,
+    MENU_BACK,
+
+    SELECT_BEATMAP,
+
+    PAUSE_HOVER,
+    PAUSE_CLICK,
+
+    KEY_DELETE,
+    KEY_PRESS,
+
+    SELECT_EXPAND,
+}
