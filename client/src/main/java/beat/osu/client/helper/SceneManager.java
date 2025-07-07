a8e6f1a7e38a6bc686dc628e555cef448ae3d35d
Artificed <paultsai.id@gmail.com>
2025-07-07 10:30:49 +0700

    revert: Made scene fullscreen

--- client/src/main/java/beat/osu/client/helper/SceneManager.java
+++ client/src/main/java/beat/osu/client/helper/SceneManager.java
@@ -6,6 +6,7 @@ import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import lombok.Getter;
@@ -36,8 +37,8 @@ public class SceneManager {
     private void setupStage() {
         stage.setTitle("BeaTOsu!");
         stage.setFullScreenExitHint("");
-//        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-//        stage.setResizable(false);
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.setResizable(false);
         stage.setFullScreen(true);
 
         stage.getIcons().add(new Image(Objects.requireNonNull(Main.class
